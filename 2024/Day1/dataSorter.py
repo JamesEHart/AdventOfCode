0381ff95c28d142d80d3c0cc0e1e3d53e0e94ee5
JamesEHart <jhart27@dtechhs.org>
2025-03-21 08:36:46 -0700

    Day 1, Part 2

--- 2024/Day1/dataSorter.py
+++ 2024/Day1/dataSorter.py
@@ -2,6 +2,8 @@ file_path = 'C:/Users/james/GitHub/AdventOfCode/2024/Day1/data.txt'
 
 first_five = []
 last_five = []
+count = 0
+finalValue = 0
 totalDistance = 0
 
 with open(file_path, 'r') as file:
@@ -9,15 +11,17 @@ with open(file_path, 'r') as file:
         first_five.append(line[:5])
         last_five.append(line[8:13])
 
-        
 first_five.sort()
 last_five.sort()
 
-# print("FIRST FIVE:" + str(first_five))
-# print("LAST FIVE:" + str(last_five))
-
-
 for i in range (len(first_five)):
     totalDistance += abs(int(first_five[i]) - int(last_five[i]))
+    for x in range (len(last_five)):
+        if (int(last_five[x]) == int(first_five[i])):
+            count += 1
 
-print(totalDistance)
+    finalValue += int(first_five[i]) * count
+    count = 0
+
+print("(Part 1) The total distance is: " + str(totalDistance))
+print("(Part 2) The similarity value is: " + str(finalValue))
